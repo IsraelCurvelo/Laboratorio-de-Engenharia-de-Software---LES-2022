2df674140a8c63a87f31377f1f49d636f96d0701
Israel Curvelo <israel.curvelo@bizapp.com.br>
2022-05-31 20:48:06 -0300

    Criado ProdutoViewModel

--- RaellShoes/Models/ViewModel/ProdutoViewModel.cs
+++ RaellShoes/Models/ViewModel/ProdutoViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RaellShoes.Models.Administrador;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,6 +8,21 @@ namespace RaellShoes.Models.ViewModel
 {
     public class ProdutoViewModel
     {
+        public Produto Produto { get; set; }
+        public Categoria Categoria { get; set; }
+        public List<Categoria> Categorias { get; set; }
+
+        public Fornecedor Fornecedor { get; set; }
+        public List<Fornecedor> Fornecedores { get; set; }
+        public GrupoPrecificacao GrupoPrecificacao { get; set; }
+        public List<GrupoPrecificacao> GrupoPrecificacaos { get; set; }
+
+        public ProdutoViewModel()
+        {
+            Categorias = new List<Categoria>();
+            Fornecedores = new List<Fornecedor>();
+            GrupoPrecificacaos = new List<GrupoPrecificacao>();
+        }
     }
 }
 
